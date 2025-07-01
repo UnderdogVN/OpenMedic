4c3a7fcbc2d6ec6afdf9d83553c2ce8fb7767748
DatacollectorVN <datacollectoriu2@gmail.com>
2025-07-01 19:24:51 +0700

    [86eu0yxqr] Added execute_eval_per_epoch method

--- src/openmedic/core/shared/services/plans/management.py
+++ src/openmedic/core/shared/services/plans/management.py
@@ -33,25 +33,25 @@ class OpenMedicPipelineBase(ABC):
 
 
 class OpenMedicPipeline:
-    MODULE_TEMPLATE: str = "openmedic.core.pipelines.{pipeline_name}"
+    # MODULE_TEMPLATE: str = "openmedic.core.pipelines.{pipeline_name}"
 
-    @classmethod
-    def execute(cls, pipeline_name: str) -> dict:
-        """The abstract method for all pipelines in `openmedic.core.pipelines`."""
-        args: argparse.Namespace
-        parser: argparse.ArgumentParser
-        pipeline: OpenMedicPipelineBase = utils.import_module(
-            module_name=cls.MODULE_TEMPLATE.format(
-                pipeline_name=pipeline_name,
-            )
-        )
-        parser = pipeline.init_arguments()
-        args, _ = parser.parse_known_args()
-        kwargs = {
-            attr: getattr(args, attr) for attr in dir(args) if not attr.startswith("_")
-        }
-        kwargs["pipeline_name"] = pipeline_name
-        return pipeline.run(**kwargs)
+    # @classmethod
+    # def execute(cls, pipeline_name: str) -> dict:
+    #     """The abstract method for all pipelines in `openmedic.core.pipelines`."""
+    #     args: argparse.Namespace
+    #     parser: argparse.ArgumentParser
+    #     pipeline: OpenMedicPipelineBase = utils.import_module(
+    #         module_name=cls.MODULE_TEMPLATE.format(
+    #             pipeline_name=pipeline_name,
+    #         )
+    #     )
+    #     parser = pipeline.init_arguments()
+    #     args, _ = parser.parse_known_args()
+    #     kwargs = {
+    #         attr: getattr(args, attr) for attr in dir(args) if not attr.startswith("_")
+    #     }
+    #     kwargs["pipeline_name"] = pipeline_name
+    #     return pipeline.run(**kwargs)
 
     @classmethod
     def _get_all_pipeline_info(cls) -> dict:
@@ -84,15 +84,15 @@ class OpenMedicPipeline:
 
     @classmethod
     def get_pipeline_info(cls, mode: Optional[str]=None) -> dict:
-        pipline_info: dict = cls._get_all_pipeline_info()
+        pipeline_info: dict = cls._get_all_pipeline_info()
         if not mode:
-            return pipline_info
+            return pipeline_info
 
         method_name = f"_get_{mode}_pipeline_info"
         method: any = getattr(cls, method_name, None)
         if not method:
             raise OpenMedicExeception(f"[OpenMedicPipeline][get_pipeline_config]: No method found `{method_name}`")
-        return method(pipline_info=pipline_info)
+        return method(pipeline_info=pipeline_info)
 
 
 class OpenMedicManager:
@@ -230,11 +230,11 @@ class OpenMedicManager:
             num_workers=self.pipeline_info["num_workers"]
         )
 
-        self.model, self.optimizer = self.open_trainer.get_object(
+        self.open_model, self.optimizer = self.open_trainer.get_object(
             names=["model", "optimizer"]
         )
         if self.pipeline_info["is_gpu"]:
-            model = model.to(device=self.device)
+            self.open_model = self.open_model.to(device=self.device)
 
     def activate_train(self):
         """Activate train mode."""
@@ -263,44 +263,41 @@ class OpenMedicManager:
             open_manager.plan_train(
                 config_path=config_path
             )
-
             n_epochs: int = open_manager.pipeline_info["n_epochs"]
-            train_losses: list = []
-            train_metric_scores: list = []
             for epoch in range(1, n_epochs + 1):
                 open_manager.activate_train()
-                result: List[float] = open_manager.execute_train_per_epoch(epoch=epoch, verbose=True)
-                train_losses.append(result[0])
-                train_metric_scores.append(result[1])
+                train_loss: float
+                train_metric_score: float
+                train_loss, train_metric_score = open_manager.execute_train_per_epoch(epoch=epoch, verbose=True)
         ```
         """
         step: int
         images: torch.Tensor
         gts: torch.Tensor
-        losses: torch.Tensor
+        loss: torch.Tensor
         metric_score: float
-        step_train_metric_scores: list = []
-        step_train_losses: list = []
+        train_metric_scores: list = []
+        train_losses: list = []
         for step, (images, gts) in enumerate(self.train_loader, 1):
             images, gts = self._process_batch(
                 images=images,
                 gts=gts,
             )
             self.optimizer.zero_grad()
-            losses, metric_score = self.open_trainer.feedforward(images=images, gts=gts)
-            step_train_metric_scores.append(metric_score)
-            step_train_losses.append(losses.item())
-            losses.backward()
+            loss, metric_score = self.open_trainer.feedforward(images=images, gts=gts)
+            train_metric_scores.append(metric_score)
+            train_losses.append(loss.item())
+            loss.backward()
             self.optimizer.step()
 
             if step % 10 == 0 and verbose:
                 print(
-                    f"\r\tTraining in step {step} with loss {statistics.mean(step_train_losses):.5f} and metric score: {statistics.mean(step_train_metric_scores):.5f}",
+                    f"\r\tTraining in step {step} with loss {statistics.mean(train_losses):.5f} and metric score: {statistics.mean(train_metric_scores):.5f}",
                     end='',
                     flush=True
                 )
-        train_loss_per_step: float = statistics.mean(step_train_losses)
-        train_metric_score_per_step: float = statistics.mean(step_train_metric_scores)\
+        train_loss_per_step: float = statistics.mean(train_losses)
+        train_metric_score_per_step: float = statistics.mean(train_metric_scores)
 
         if verbose:
             print(
@@ -316,5 +313,66 @@ class OpenMedicManager:
     def plan_eval(self, config_path: str):
         pass
 
-    def execute_eval(self):
-        pass
+    def execute_eval_per_epoch(self, epoch: int, verbose: bool=True) -> List[float]:
+        """Execute training process per epoch.
+
+        Input:
+        ------
+            epoch: int - The current epoch.
+            verbose: bool - Manifests the log if True.
+                By default is True.
+
+        Output:
+        -------
+            train_loss_per_step, train_metric_score_per_step: List[float].
+                The loss and metric score per step.
+        Usage:
+        ------
+        ```
+            open_manager.plan_eval(
+                config_path=config_path
+            )
+            # Or open_manager.plan_train(...)
+
+            n_epochs: int = open_manager.pipeline_info["n_epochs"]
+            for epoch in range(1, n_epochs + 1):
+                open_manager.activate_eval()
+                eval_loss: float
+                eval_metric_score: float
+                eval_loss, eval_metric_score = open_manager.execute_eval_per_epoch(epoch=epoch, verbose=True)
+        ```
+        """
+        step: int
+        images: torch.Tensor
+        gts: torch.Tensor
+        loss: torch.Tensor
+        metric_score: float
+        eval_metric_scores: list = []
+        eval_losses: list = []
+        with torch.no_grad():
+            for step, (images, gts) in enumerate(self.train_loader, 1):
+                images, gts = self._process_batch(
+                    images=images,
+                    gts=gts,
+                )
+                loss, metric_score = self.open_trainer.feedforward(images=images, gts=gts)
+                eval_metric_scores.append(metric_score)
+                eval_losses.append(loss.item())
+
+                if step % 10 == 0 and verbose:
+                    print(
+                        f"\r\tEvaluating in step {step} with loss {statistics.mean(eval_losses):.5f} and metric score: {statistics.mean(eval_metric_scores):.5f}",
+                        end='',
+                        flush=True
+                    )
+            eval_loss_per_step: float = statistics.mean(eval_losses)
+            eval_metric_score_per_step: float = statistics.mean(eval_metric_scores)
+            if verbose:
+                print(
+                    f"\r\tCompleted evaluation at epoch {epoch} with loss {eval_loss_per_step:.5f} and metric score: {eval_metric_score_per_step:.5f}",
+                    flush=True
+                )
+
+        return eval_loss_per_step, eval_metric_score_per_step
+
+
