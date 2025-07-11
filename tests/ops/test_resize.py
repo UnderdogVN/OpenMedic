6b070491c0fccd4af04ef348d5b7fdc5c717e630
DatacollectorVN <datacollectoriu2@gmail.com>
2025-07-11 13:08:50 +0700

    [86eu0fq2y] rellocated test_resize.py

--- tests/ops/test_resize.py
+++ tests/ops/test_resize.py
@@ -4,9 +4,9 @@ from openmedic.core.shared.services.objects.ops.transformers.resize import Resiz
 
 def test_resize_execute():
     # Create dummy image and ground truth
-    image = np.random.randint(0, 255, (128, 128, 3), dtype=np.uint8)
-    gt = np.random.randint(0, 2, (128, 128), dtype=np.uint8)
-    resize_op = Resize.initialize(target_w=100, target_h=100, interpolation='INTER_LINEAR')
+    image: np.ndarray = np.random.randint(0, 255, (128, 128, 3), dtype=np.uint8)
+    gt: np.ndarray = np.random.randint(0, 2, (128, 128), dtype=np.uint8)
+    resize_op: Resize = Resize.initialize(target_w=100, target_h=100, interpolation='INTER_LINEAR')
     resized_image, resized_gt = resize_op.execute(image, gt)
     assert resized_image.shape == (100, 100, 3)
     assert resized_gt.shape == (100, 100)
